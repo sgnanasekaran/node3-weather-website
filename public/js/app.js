22e46cd3c182353ca4818a8df9c4c2809699e553
Suresh Gnanasekaran <skg@Sureshs-MBP.home>
2021-11-25 11:57:46 -0500

    Removed the console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,3 @@
-console.log ("Hello from Javascript file")
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-
-// })
-
-
-
 const weatherForm = document.querySelector('form')
 
 const search = document.querySelector('input')
